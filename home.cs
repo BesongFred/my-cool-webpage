8759d5d8c6532abde979bfb781d6bd895ba8fff9
BesongFred <167295276+BesongFred@users.noreply.github.com>
2025-03-05 09:08:42 +0100

    Add files via upload

---
+++ home.cs
@@ -0,0 +1,104 @@
+body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    background-image: url('house22\ \(4\).JPG');
+    background-size: contain;
+    background-position: center;
+}
+
+  
+
+header {
+    background-color: #333;
+    color: #fff;
+    padding: 1em;
+    text-align: center;
+}
+
+header nav ul {
+    list-style: none;
+    margin: 0px;
+    padding: 0px;
+    display: flex;
+    justify-content: space-evenly;
+}
+
+header nav ul li {
+    margin-right: 0px;
+}
+
+header nav a {
+    color: #fff;
+    text-decoration: none;
+}
+
+.logo {
+    width: 100px;
+    height: 100px;
+    float: left;
+    margin-top: 0px;
+    border-radius: 20px;
+}
+
+main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 2em;
+}
+
+.hero {
+    background-color: #fff;
+    padding: 2em;
+    border-radius: 50px;
+    box-shadow: 0 0 10px rgba(185, 22, 22, 0.1);
+}
+
+.hero h1 {
+    font-size: 36px;
+    margin-bottom: 10px;
+}
+
+.features {
+    margin-top: 20px;
+}
+
+.features ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    
+}
+
+.features li {
+    margin-bottom: 10px;
+    background-color: #fff;
+}
+
+.features i {
+    margin-right: 10px;
+}
+.testimonials {
+    margin-top: 20px;
+}
+
+.testimonials ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    background-color: #fff;
+}
+
+.testimonials li {
+    margin-bottom: 20px;
+    background-color: #fff;
+}
+
+footer {
+    background-color: #333;
+    color: #fff;
+    padding: 1em;
+    text-align: center;
+    clear: both;
+}
